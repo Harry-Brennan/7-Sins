9ded03804159288f60fa89de99aa13297e8b72c4
Harry Brennan <harrymb04@hotmail.co.uk>
2025-04-27 12:01:30 +0100

    Attempting to fix the gitignore.

--- Project_7_Sins/Intermediate/Build/Win64/x64/Project_7_SinsEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "C:/Users/ASUS/Documents/GitHub/7-Sins/Project_7_Sins/Intermediate/Build/Win64/x64/Project_7_SinsEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
